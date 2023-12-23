c2b14c937d6d9188b6cb09de5d67be52507e6c45
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2023-12-23 00:32:18 +0000

    Put all item, audio and sample asset handling to the AssetBundleHandler script, got rid of some warnings and refactored setup functions to not require the asset bundle as parameter (since only the handler should worry about that) and limited them to just call their respective upgrade names

--- MoreShipUpgrades/Misc/AssetBundleHandler.cs
+++ MoreShipUpgrades/Misc/AssetBundleHandler.cs
@@ -13,6 +13,16 @@ namespace MoreShipUpgrades.Misc
     {
         private static string MODULE_NAME = "Asset Bundle Handler";
         private static Dictionary<string, string> infoJSON;
+        public static Dictionary<string, string> samplePaths = new Dictionary<string, string>()
+        {
+            { "snare flea", "Assets/ShipUpgrades/Samples/SnareFleaSample.asset" },
+            { "bunker spider", "Assets/ShipUpgrades/Samples/BunkerSpiderSample.asset" },
+            { "hoarding bug", "Assets/ShipUpgrades/Samples/HoardingBugSample.asset" },
+            { "bracken", "Assets/ShipUpgrades/Samples/BrackenSample.asset" },
+            { "eyeless dog", "Assets/ShipUpgrades/Samples/EyelessDogSample.asset" },
+            { "baboon hawk", "Assets/ShipUpgrades/Samples/BaboonHawkSample.asset" },
+            { "thumper", "Assets/ShipUpgrades/Samples/ThumperSample.asset" },
+        };
         private static Dictionary<string, string> assetPaths = new Dictionary<string, string>()
         {
             { "Beekeeper", "Assets/ShipUpgrades/beekeeper.prefab" },
@@ -33,6 +43,18 @@ namespace MoreShipUpgrades.Misc
             { "Fast Encryption", "Assets/ShipUpgrades/Pager.prefab" },
             { "Locksmith", "Assets/ShipUpgrades/LockSmith.prefab" },
             { playerHealthScript.UPGRADE_NAME, "Assets/ShipUpgrades/PlayerHealth.prefab" },
+
+            { "Advanced Portable Tele", "Assets/ShipUpgrades/TpButtonAdv.asset" },
+            { "Portable Tele", "Assets/ShipUpgrades/TpButton.asset" },
+            { "Peeper", "Assets/ShipUpgrades/coilHead.asset" },
+            { "Medkit", "Assets/ShipUpgrades/MedKitItem.asset" },
+            { "Night Vision", "Assets/ShipUpgrades/NightVisionItem.asset" },
+            { "Medkit", "Assets/ShipUpgrades/MedKitItem.asset" },
+
+            { "Break", "Assets/ShipUpgrades/break.mp3" },
+            { "Error", "Assets/ShipUpgrades/error.mp3" },
+            { "Button Press", "Assets/ShipUpgrades/ButtonPress2.ogg" },
+            { "Flashbang", "Assets/ShipUpgrades/flashbangsfx.ogg" },
         };
         /// <summary>
         /// Tries to load an asset from provided asset bundle through a given path into a GameObject
@@ -50,6 +72,7 @@ namespace MoreShipUpgrades.Misc
             {
                 Plugin.mls.LogError(string.Format("[{0}] An error has occurred trying to load asset from {1}\n", MODULE_NAME, path));
             }
+            Plugin.mls.LogInfo(string.Format("[{0}] Loaded asset located in {1}", MODULE_NAME, path));
             return result;
         }
         /// <summary>
@@ -68,6 +91,7 @@ namespace MoreShipUpgrades.Misc
             {
                 Plugin.mls.LogError(string.Format("[{0}] An error has occurred trying to load asset from {1}\n", MODULE_NAME, path));
             }
+            Plugin.mls.LogInfo(string.Format("[{0}] Loaded asset located in {1}", MODULE_NAME, path));
             return result;
         }
 
@@ -87,6 +111,7 @@ namespace MoreShipUpgrades.Misc
             {
                 Plugin.mls.LogError(string.Format("[{0}] An error has occurred trying to load asset from {1}\n", MODULE_NAME, path));
             }
+            Plugin.mls.LogInfo(string.Format("[{0}] Loaded asset located in {1}", MODULE_NAME, path));
             return result;
         }
 
@@ -127,10 +152,29 @@ namespace MoreShipUpgrades.Misc
         {
             if (!assetPaths.ContainsKey(upgradeName)) 
             {
-                Plugin.mls.LogError(string.Format("[{0}] The key was not present in the asset path dictionary!\n", MODULE_NAME));
+                Plugin.mls.LogError(string.Format("[{0}] {1} was not present in the asset dictionary!\n", MODULE_NAME, upgradeName));
                 return null;
             }
             return TryLoadGameObjectAsset(ref UpgradeBus.instance.UpgradeAssets, assetPaths[upgradeName]);
+        }
+
+        public static Item GetItemObject(string itemName)
+        {
+            if (assetPaths.ContainsKey(itemName)) return TryLoadItemAsset(ref UpgradeBus.instance.UpgradeAssets, assetPaths[itemName]);
+            if (samplePaths.ContainsKey(itemName)) return TryLoadItemAsset(ref UpgradeBus.instance.UpgradeAssets, samplePaths[itemName]);
+
+            Plugin.mls.LogError(string.Format("[{0}] {1} was not present in the asset or sample dictionary!\n", MODULE_NAME, itemName));
+            return null;
+        }
+
+        public static AudioClip GetAudioClip(string audioName)
+        {
+            if (!assetPaths.ContainsKey(audioName))
+            {
+                Plugin.mls.LogError(string.Format("[{0}] {1} was not present in the asset dictionary!\n", MODULE_NAME, audioName));
+                return null;
+            }
+            return TryLoadAudioClipAsset(ref UpgradeBus.instance.UpgradeAssets, assetPaths[audioName]);
         }
     }
 }
