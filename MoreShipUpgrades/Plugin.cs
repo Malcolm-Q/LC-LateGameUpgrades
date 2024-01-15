6c6e3e27cbd6c500fbe1c6196afb56a46157c7e1
Malcolm Kew <malcolmthekew@gmail.com>
2024-01-14 19:30:29 -0700

    idk

--- MoreShipUpgrades/Plugin.cs
+++ MoreShipUpgrades/Plugin.cs
@@ -588,7 +588,6 @@ namespace MoreShipUpgrades
             barrowScript.wheelsClip = shoppingCartSound;
             LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(wheelbarrow.spawnPrefab);
 
-            LethalLib.Modules.Items.RegisterItem(wheelbarrow);
             if (!cfg.SCRAP_WHEELBARROW_ENABLED) return; 
             AnimationCurve curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(cfg.SCRAP_WHEELBARROW_RARITY, 1), new Keyframe(1, 1));
             SpawnableMapObjectDef mapObjDef = ScriptableObject.CreateInstance<SpawnableMapObjectDef>();
