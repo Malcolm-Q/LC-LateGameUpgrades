1aefae565a26b644b4fa7dc6f65372279f35faac
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-01-16 17:09:13 +0000

    Forgot newlines at the end

--- MoreShipUpgrades/Patches/TerminalPatcher.cs
+++ MoreShipUpgrades/Patches/TerminalPatcher.cs
@@ -14,8 +14,8 @@ namespace MoreShipUpgrades.Patches
         private const string EXTEND_HELP_COMMAND = ">EXTEND DEADLINE <DAYS>\nExtends the deadline by specified amount. Consumes {0} for each day extended.\n\n";
         private const string INTERNS_HELP_COMMAND = ">INTERNS / INTERN \nRevives the selected player in radar with a new employee. Consumes {0} credits for each revive.\n\n";
         private const string CONTRACT_HELP_COMMAND = ">CONTRACT \nGives you a random contract for a scrap item with considerable value and lasts til you leave from assigned planet.\nConsumes {0} credits for each contract and will be unable to get another contract til current has expired.\n\n";
-        private const string ATK_HELP_COMMAND = ">ATK / INITATTACK \n Stuns nearby enemies for a set period of time. Only applicable when Discombobulator has been purchased";
-        private const string CD_HELP_COMMAND = ">CD / COOLDOWN \n Shows the current cooldown of the ship stun ability. Only applicable when Discombobulator has been purchased";
+        private const string ATK_HELP_COMMAND = ">ATK / INITATTACK \n Stuns nearby enemies for a set period of time. Only applicable when Discombobulator has been purchased\n\n";
+        private const string CD_HELP_COMMAND = ">CD / COOLDOWN \n Shows the current cooldown of the ship stun ability. Only applicable when Discombobulator has been purchased\n\n";
         [HarmonyPostfix]
         [HarmonyPatch("Start")]
         private static void StartPostfix(ref Terminal __instance)
