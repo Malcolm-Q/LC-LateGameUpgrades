593b2c7e909bb764572ba59a407961dd5f199cff
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-02-08 08:46:49 +0000

    Corrected method call placement for Exterminator loot item and changed ScrapValueSyncer logic

--- MoreShipUpgrades/Misc/ScrapValueSyncer.cs
+++ MoreShipUpgrades/Misc/ScrapValueSyncer.cs
@@ -6,25 +6,9 @@ namespace MoreShipUpgrades.Misc
 {
     internal class ScrapValueSyncer : MonoBehaviour
     {
-        int scrapValue = -1;
-        void Start()
-        {
-            StartCoroutine(waitTilDefinedScrapValue());
-        }
         internal void SetScrapValue(int scrapValue)
         {
-            this.scrapValue = scrapValue;
-        }
-        bool IsScrapValueNotSet()
-        {
-            return scrapValue < 0;
-        }
-        IEnumerator waitTilDefinedScrapValue()
-        {
-            yield return new WaitWhile(IsScrapValueNotSet); // This should not get stuck if we always set the value right after instantianting the gameObject
-            
             GrabbableObject prop = GetComponent<GrabbableObject>();
-            if (prop.scrapValue > 0) yield break; // Already defined
             prop.scrapValue = scrapValue;
 
             ScanNodeProperties node = GetComponentInChildren<ScanNodeProperties>();
