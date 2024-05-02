fc33003fbf1731c5592a5ec8afc9baa92dac7889
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-05-02 01:15:52 +0100

    Removing features replaced by Interactable UIs

--- MoreShipUpgrades/Managers/WeatherManager.cs
+++ MoreShipUpgrades/Managers/WeatherManager.cs
@@ -6,23 +6,6 @@ namespace MoreShipUpgrades.Managers
     internal static class WeatherManager
     {
         internal const string WEATHER_PROBE_COMMAND = "Weather Probe";
-        internal static (string, LevelWeatherType) PickWeather(string levelName, string weather = "")
-        {
-            SelectableLevel[] availableLevels = StartOfRound.Instance.levels;
-            SelectableLevel selectedLevel = Array.Find(availableLevels, x => x.PlanetName.ToLower().Contains(levelName) && !x.PlanetName.Contains("Gordion"));
-            if (selectedLevel == null) return (null, LevelWeatherType.None);
-            switch (weather)
-            {
-                case "clear":
-                case "none":
-                    {
-                        return (selectedLevel.PlanetName, LevelWeatherType.None);
-                    }
-                case "": return RandomizeWeather(ref selectedLevel);
-            }
-            LevelWeatherType newSelectedWeather = selectedLevel.randomWeathers.Select(x => x.weatherType).FirstOrDefault(x => x.ToString().ToLower().Contains(weather));
-            return (selectedLevel.PlanetName, newSelectedWeather);
-        }
 
         internal static (string, LevelWeatherType) RandomizeWeather(ref SelectableLevel level)
         {
